9d2fdad8f16a4283a41cb66fd7ac86743b6fee7c
Vitor Almeida <vitor.almeida.dev@gmail.com>
2022-02-18 22:11:38 -0300

    feat: add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { AccessToken } from 'domain/models'
+import { AuthenticationError } from 'domain/errors'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.result>
+}
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type result = AccessToken | AuthenticationError
+}
